58db83d21a150524ae549894a4ce4b84f33b48ad
youssef-salih <115431342+youssef-salih@users.noreply.github.com>
2023-07-16 21:56:50 +0100

    2 p

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -36,6 +36,7 @@ const Card = ({ src, thumb, name }) => {
         <div className="flex flex-col">
           <a href="#">
             <p className="text-gray-100 text-sm font-semibold">{name}</p>
+            <p className="text-gray-100 text-sm font-semibold">{name}</p>
           </a>
         </div>
       </div>
