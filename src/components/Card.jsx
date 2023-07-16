24b4066e1efcce5bc4f7eb4cee7ff5c61f5e2274
youssef-salih <115431342+youssef-salih@users.noreply.github.com>
2023-07-16 23:16:55 +0100

    firebase

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useState } from "react";
 
 const Card = ({ src, thumb, name }) => {
   const [isPlaying, setIsPlaying] = useState(false);
